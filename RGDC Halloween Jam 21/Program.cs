3860b3ad78061db55ab8187d5223b0f483260e39
William <wcd6573@rit.edu>
2021-10-30 15:15:44 -0400

    Wrote out the starting stuff (up to naming the creature).

--- RGDC Halloween Jam 21/Program.cs
+++ RGDC Halloween Jam 21/Program.cs
@@ -1,14 +1,48 @@
 ﻿using System;
 
-namespace RGDC_Halloween_Jam_21
+/*
+ * Text-Based Game because that's all we know how to do
+ * Sacrificed visuals and gameplay (clever and lazy)
+ */
+
+namespace GameJamTextGame
 {
-    public static class Program
+    class Program
     {
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            using (var game = new Game1())
-                game.Run();
+            string name;
+            string fruit;
+            ConsoleKeyInfo key;
+
+            Console.WriteLine(
+                "You awaken in a strange, barren land, devoid of all colorful\n" +
+                "graphics, intuitive user interfaces, and enjoyable gameplay.\n" +
+                "The developers sacrificed these things because they didn't\n" +
+                "know how to implement them effectively.\n" +
+                "Press [L] to look around."
+                );
+            do
+            {
+                key = Console.ReadKey(true);
+            } while (key.KeyChar != 'l');
+
+            Console.WriteLine(
+                "All that can be seen is this strange creature.\n" +
+                "[ :( ]\n" +
+                "It appears sad, but upon seeing you, it smiles.\n" +
+                "[ :) ]\n" +
+                "It approaches you.");
+
+            Console.Write("Give it a name: ");
+            name = Console.ReadLine();
+            Console.WriteLine(
+                "You say the new name aloud, and {0} beams.\n" +
+                "[ :)) ]\n" +
+                "Literally, beams of light emerge from the creature, clearing\n" +
+                "the fog of the console window.",
+                name);
+
         }
     }
 }
