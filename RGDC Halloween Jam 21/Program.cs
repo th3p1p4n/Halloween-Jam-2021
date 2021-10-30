edc1b4dfb7d48c42bc5fa74831595cf8feebca61
th3p1p4n <mjba504@rit.edu>
2021-10-29 20:28:08 -0400

    first

---
+++ RGDC Halloween Jam 21/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RGDC_Halloween_Jam_21
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
